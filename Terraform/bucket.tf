e8c999048a090ec328c3477a71d700163e9c5117
Raphael Bottino <raphael.bottino@sentinelone.com>
2025-01-06 09:32:26 -0600

    Fixing AWS S3 Buckets not encrypted With KMS Customer Managed Key (CMK)

--- Terraform/bucket.tf
+++ Terraform/bucket.tf
@@ -19,6 +19,15 @@ resource "aws_s3_bucket" "secure_bucket" {
   #   }
   # }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"  # Use AWS KMS encryption
+        kms_master_key_id = "arn:aws:kms:us-west-2:123456789012:key/abcd1234-5678-90ab-cdef-ghijklmnopqr"  # Sample KMS Key ARN
+      }
+    }
+  }
+
   # Block all public access
   block_public_acls   = false
   block_public_policy = false
@@ -81,4 +90,20 @@ resource "aws_s3_bucket_policy" "secure_bucket_policy" {
       }
     ]
   })
+}
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
